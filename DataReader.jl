d0530be3879d711daf65bad0eecd005015ad0787
alikaratas17 <alikaratas17@ku.edu.tr>
2021-11-07 22:56:00 +0300

    Added a data reader for tsv data files

--- DataReader.jl
+++ DataReader.jl
@@ -1,7 +1,20 @@
-export readData
+export readData, readData_tsv
 using JSON
 function readData(filename)
     io = open(filename,"r")
     s = readlines(io)
     return JSON.parse.(s);
+end
+function readData_tsv(filename)
+    io = open(filename,"r")
+    s = readlines(io)
+    labels = split(s[1],"\t")
+    indices = labels .!= ""
+    labels = labels[indices]
+    l = []
+    for i = 2:length(s)
+       line = split(s[i],"\t")[indices]
+       push!(l,Dict(zip(labels,line))) 
+    end
+    return l;
 end
